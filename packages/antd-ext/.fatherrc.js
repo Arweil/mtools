9c5c822e109c2c6aab6c85baef9a4a80cb0aeb45
chenchangyu <chenchangyu@nuanwa.net>
2023-05-09 16:10:23 +0800

    feat: eslint prettier stylelint commitlint

---
+++ packages/antd-ext/.fatherrc.js
@@ -0,0 +1,8 @@
+export default {
+  // 以下为 esm 配置项启用时的默认值，有自定义需求时才需配置
+  esm: {
+    input: 'components', // 默认编译目录
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
+  },
+};
