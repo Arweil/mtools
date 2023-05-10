cc85f24e213cdd9aebeecc64077ba4a8360864a4
chenchangyu <chenchangyu@nuanwa.net>
2023-05-10 10:12:25 +0800

    feat: 更改publicPath

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,13 @@
 // 配置文件
-export default {
+import { defineConfig } from 'dumi';
+import { resolve } from 'path';
+
+export default defineConfig({
   title: 'm-tools',
   mode: 'site',
   exportStatic: {},
+  base: '/mtools',
+  publicPath: '/mtools/',
   hash: true,
   navs: [
     null,
@@ -10,6 +15,6 @@ export default {
     { title: '更新日志', path: 'https://github.com/Arweil/mtools/releases' },
   ],
   alias: {
-    'antd-ext': __dirname + '/packages/antd-ext',
+    'antd-ext': resolve('./', 'packages/antd-ext/components/index.ts'),
   },
-};
+});
