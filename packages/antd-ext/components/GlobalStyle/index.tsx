2505e6dd3a8e1c7bd2dc89d9b4975963a8a1f191
Milk <luming@nuanwa.net>
2024-03-15 09:47:29 +0800

    feat(antd-ext): notification 兼容zeus

--- packages/antd-ext/components/GlobalStyle/index.tsx
+++ packages/antd-ext/components/GlobalStyle/index.tsx
@@ -2,15 +2,17 @@ import React, { useContext } from 'react';
 import { AntdExtGlobalContext } from '../ConfigProviderExt/context';
 import { NotificationGlobalStyle } from '../Notification';
 import { usePrefixCls } from '../utils';
+import useTokenExt from '../utils/useTokenExt';
 
 export default function GlobalStyle() {
   const { prefixCls } = usePrefixCls();
   const { themeExt } = useContext(AntdExtGlobalContext);
   const finTheme = themeExt;
+  const tokenExt = useTokenExt(finTheme);
 
   return (
     <>
-      <NotificationGlobalStyle prefixCls={prefixCls} theme={finTheme} />
+      <NotificationGlobalStyle prefixCls={prefixCls} theme={finTheme} tokenExt={tokenExt} />
     </>
   );
 }
